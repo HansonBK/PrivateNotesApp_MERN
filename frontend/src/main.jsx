7fd3a591b5370eecc81299bd198098d6f1973f46
HansonBK <hansonhans88@gmail.com>
2026-01-19 14:39:33 -0500

    Frontend install dependencies and set a theme

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {BrowserRouter} from "react-router"
+import {Toaster} from "react-hot-toast"
+import React from 'react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    
+      <div className="min-h-screen bg-gradient-to-r from-[#94BBE9] via-[#EEAECA] to-[#94BBE9] p-10">
+        <BrowserRouter>
+          <App />
+         </BrowserRouter>
+      </div>
+   
   </StrictMode>,
 )
